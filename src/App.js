4525b804d513ccb4ac786cef1a9c82da9ad32a31
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-24 12:39:42 -0400

    added extra css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+import "./App.css";
 
-class App extends React.Component {
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
   render() {
+    const filterRobots = this.state.robots.filter(robots => {
+      return robots.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filterRobots}/>
       </div>
-    );
+    )
   }
 }
+
 export default App;
